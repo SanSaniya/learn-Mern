4548d2244e8c62b7ae3d20693af6c7eec65e2c13
SanSaniya <158427968+SanSaniya@users.noreply.github.com>
2024-05-13 09:45:10 +0530

    Add files via upload

---
+++ mini.java
@@ -0,0 +1,6 @@
+public class mini{
+    public static void main(String[] args) {
+        System.out.println("Happy has been here before");
+        System.out.println("ButterCup has new duty of moving forward");
+    }
+}
